34876a6e8ee3a202117dbee901faf5849355c3e2
halillusion <halillusion@gmail.com>
2023-08-16 02:47:30 +0300

    external files

--- app/External/endpoints.php
+++ app/External/endpoints.php
@@ -7,13 +7,215 @@
 
 
 return [
-	'hesap' => [
+	// Basic Auth
+	'auth' => [
 		'default' => true,
 		'name' => 'auth.auth',
 	],
-
-	'hesap/cikis' => [
+	'auth/:action' => [
+		'default' => true,
+		'name' => 'auth.auth_action',
+	],
+	'auth/logout' => [
 		'default' => true,
 		'name' => 'auth.auth_logout',
+	],
+
+	// Management
+	'management' => [
+		'default' => false,
+		'name' => 'auth.management',
+	],
+	'management/users' => [
+		'default' => false,
+		'name' => 'auth.management_users',
+	],
+	'management/users/list' => [
+		'default' => false,
+		'name' => 'auth.management_users_list',
+	],
+	'management/users/add' => [
+		'default' => false,
+		'name' => 'auth.management_users_add',
+	],
+	'management/users/:id' => [
+		'default' => false,
+		'name' => 'auth.management_users_detail',
+	],
+	'management/users/:id/update' => [
+		'default' => false,
+		'name' => 'auth.management_users_update',
+	],
+	'management/users/:id/delete' => [
+		'default' => false,
+		'name' => 'auth.management_users_delete',
+	],
+	'management/roles' => [
+		'default' => false,
+		'name' => 'auth.management_roles',
+	],
+	'management/roles/list' => [
+		'default' => false,
+		'name' => 'auth.management_roles_list',
+	],
+	'management/roles/add' => [
+		'default' => false,
+		'name' => 'auth.management_roles_add',
+	],
+	'management/roles/:id' => [
+		'default' => false,
+		'name' => 'auth.management_roles_detail',
+	],
+	'management/roles/:id/delete' => [
+		'default' => false,
+		'name' => 'auth.management_roles_delete',
+	],
+	'management/roles/:id/update' => [
+		'default' => false,
+		'name' => 'auth.management_roles_update',
+	],
+	'management/sessions' => [
+		'default' => false,
+		'name' => 'auth.management_sessions',
+	],
+	'management/sessions/list' => [
+		'default' => false,
+		'name' => 'auth.management_sessions_list',
+	],
+	'management/sessions/:id/delete' => [
+		'default' => false,
+		'name' => 'auth.management_sessions_delete',
+	],
+	'management/icon-picker' => [
+		'default' => true,
+		'name' => 'auth.management_icon_picker',
+	],
+	'management/:module' => [
+		'default' => false,
+		'name' => 'auth.management_contents',
+	],
+	'management/:module/list' => [
+		'default' => false,
+		'name' => 'auth.management_contents_list',
+	],
+	'management/:module/add' => [
+		'default' => false,
+		'name' => 'auth.management_contents_add',
+	],
+	'management/:module/:id' => [
+		'default' => false,
+		'name' => 'auth.management_contents_detail',
+	],
+	'management/:module/:id/update' => [
+		'default' => false,
+		'name' => 'auth.management_contents_update',
+	],
+	'management/:module/:id/delete' => [
+		'default' => false,
+		'name' => 'auth.management_contents_delete',
+	],
+	'management/:module/slug' => [
+		'default' => true,
+		'name' => 'auth.management_contents_slug',
+	],
+	'management/:module/autocomplete' => [
+		'default' => true,
+		'name' => 'auth.management_contents_autocomplete',
+	],
+	'management/content/:module/upload-file' => [
+		'default' => false,
+		'name' => 'auth.management_content_upload_file',
+	],
+	'management/media' => [
+		'default' => false,
+		'name' => 'auth.management_media',
+	],
+	'management/media/list' => [
+		'default' => false,
+		'name' => 'auth.management_media_list',
+	],
+	'management/media/add' => [
+		'default' => false,
+		'name' => 'auth.management_media_add',
+	],
+	'management/media/:id' => [
+		'default' => false,
+		'name' => 'auth.management_media_detail',
+	],
+	'management/media/:id/update' => [
+		'default' => false,
+		'name' => 'auth.management_media_update',
+	],
+	'management/media/:id/delete' => [
+		'default' => false,
+		'name' => 'auth.management_media_delete',
+	],
+	'management/menus' => [
+		'default' => false,
+		'name' => 'auth.management_menu',
+	],
+	'management/menus/list' => [
+		'default' => false,
+		'name' => 'auth.management_menu_list',
+	],
+	'management/menus/add' => [
+		'default' => false,
+		'name' => 'auth.management_menu_add',
+	],
+	'management/menus/:id' => [
+		'default' => false,
+		'name' => 'auth.management_menu_detail',
+	],
+	'management/menus/:id/update' => [
+		'default' => false,
+		'name' => 'auth.management_menu_update',
+	],
+	'management/menus/:id/delete' => [
+		'default' => false,
+		'name' => 'auth.management_menu_delete',
+	],
+	'management/menus/get-menu-params' => [
+		'default' => false,
+		'name' => 'auth.management_menu_get_params',
+	],
+	'management/forms/:form' => [
+		'default' => false,
+		'name' => 'auth.management_forms',
+	],
+	'management/forms/:form/list' => [
+		'default' => false,
+		'name' => 'auth.management_forms_list',
+	],
+	'management/forms/:form/:id' => [
+		'default' => false,
+		'name' => 'auth.management_forms_detail',
+	],
+	'management/forms/:form/:id/update' => [
+		'default' => false,
+		'name' => 'auth.management_forms_update',
+	],
+	'management/forms/:form/:id/delete' => [
+		'default' => false,
+		'name' => 'auth.management_forms_delete',
+	],
+	'management/logs' => [
+		'default' => false,
+		'name' => 'auth.management_logs',
+	],
+	'management/logs/list' => [
+		'default' => false,
+		'name' => 'auth.management_logs_list',
+	],
+	'management/logs/:ip/block' => [
+		'default' => false,
+		'name' => 'auth.management_logs_ip_block',
+	],
+	'management/settings' => [
+		'default' => false,
+		'name' => 'auth.management_settings',
+	],
+	'management/settings/update' => [
+		'default' => false,
+		'name' => 'auth.management_settings_update',
 	],
 ];
---
+++ app/External/hook.php
@@ -0,0 +1,21 @@
+<?php
+	
+use \KX\Core\Helper;
+use \KX\Controller\ContentController;
+use \KX\Controller\FileController;
+use \KX\Controller\FormController;
+use \KX\Model\Contents as ContentsModel;
+
+/*
+  If you want to take action as a result of module-specific developments while the content controller is running, you can use hooks.
+  $type: Indicates in which method the function works. "detail" or "listing" values are displayed.
+  $container: An instance of the core class.
+  $controller: The instance of the content controller.
+  $extract: Output of the module content.
+  $output: Output from controller method. You can make changes to it.
+*/
+
+return function($type, $container, $controller, $extract, $output) {
+
+  return $output;
+}
