a0698e08d88d3786c01b04fb5f327baafe1d60e7
halillusion <halillusion@gmail.com>
2023-08-16 02:46:45 +0300

    part files

---
+++ app/View/_parts/end.php
@@ -0,0 +1,5 @@
+  <script src="<?php echo \KX\Core\Helper::assets('libs/tabler/tabler.min.js'); ?>"></script>
+  <script src="<?php echo \KX\Core\Helper::assets('js/script.js'); ?>"></script>
+  <script src="<?php echo \KX\Core\Helper::assets('js/app.js'); ?>"></script>
+  </body>
+</html>
