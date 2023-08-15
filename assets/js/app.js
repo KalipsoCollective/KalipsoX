208808a316a3a01b7d8970567d0a9e2b1119a262
halillusion <halillusion@gmail.com>
2023-08-16 02:47:00 +0300

    js

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+class KalipsoXJS {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    
+  }
+}
+
+new KalipsoXJS();
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+/* You can use this file for your own custom js code */
+
+window.frontInit = function () {
+  console.log('KalipsoX is ready!');
+}
