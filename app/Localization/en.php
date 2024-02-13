766b5edfa2290c6808ee77362f6967fd2eaf644a
halillusion <halillusion@gmail.com>
2024-02-13 17:27:04 +0300

    Add new localization strings

--- app/Localization/en.php
+++ app/Localization/en.php
@@ -25,5 +25,15 @@ return [
         'register_desc' => 'Create a new account',
         'recovery' => 'Recovery Account',
         'recovery_desc' => 'Recover your account',
-    ]
+        'email_or_username' => 'E-mail or Username',
+        'password' => 'Password',
+        'show_password' => 'Show Password',
+        'remember_me' => 'Remember Me',
+        'recovery_account' => 'Recovery Account',
+        'dont_have_account_yet' => 'Don\'t have an account yet?',
+    ],
+    'base' => [
+        'toggle_theme' => 'Toggle Theme',
+        'copyright' => 'Copyright',
+    ],
 ];
