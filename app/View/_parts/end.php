4dee4928e61bc735fa1cfb58992b33585f2ed60d
halillusion <halillusion@gmail.com>
2024-02-14 17:53:30 +0300

    Add NProgress and Toastify libraries

--- app/View/_parts/end.php
+++ app/View/_parts/end.php
@@ -1,4 +1,6 @@
     <script src="<?php echo $Helper::assets('libs/tabler/tabler.min.js'); ?>"></script>
+    <script src="<?php echo $Helper::assets('libs/nprogress/nprogress.min.js'); ?>"></script>
+    <script src="<?php echo $Helper::assets('libs/toastify/toastify.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('js/script.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('js/app.js'); ?>"></script>
     </body>
--- app/View/_parts/header.php
+++ app/View/_parts/header.php
@@ -8,7 +8,9 @@
         echo '<meta name="description" content="' . $description . '" />' . PHP_EOL;
     }   ?>
     <link href="<?php echo $Helper::assets('libs/tabler/tabler.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo $Helper::assets('libs/nprogress/nprogress.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo $Helper::assets('libs/tabler-icons/tabler-icons.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo $Helper::assets('libs/toastify/toastify.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo $Helper::assets('css/style.css'); ?>" rel="stylesheet">
     <link href="<?php echo $Helper::assets('css/app.css'); ?>" rel="stylesheet">
 </head>
