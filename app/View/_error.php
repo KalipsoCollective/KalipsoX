fde396d5f63087b5af7183cc54274e9b040fd906
halillusion <halillusion@gmail.com>
2024-02-14 16:51:36 +0300

    Add _error.php view file

---
+++ app/View/_error.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'schema' => [
+    '_parts/header',
+    'x',
+    '_parts/footer',
+    '_parts/end'
+  ],
+  'variables' => [
+    'bodyClass' => 'border-top-wide border-primary d-flex flex-column',
+  ]
+];
