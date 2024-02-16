efbc9a69f9d49dec71093120212ca3602cee1999
halillusion <halillusion@gmail.com>
2024-02-16 17:34:31 +0300

    Refactor notification hooks in notifications.php

--- app/External/notifications.php
+++ app/External/notifications.php
@@ -3,50 +3,35 @@
 /**
  * Notification Hooks 
  **/
+
 use KX\Core\Helper;
 
 return [
 
-	// User Registration Notification
-	'registration' => function($hook, $external = null) {
+    // User Registration Notification
+    'welcome' => function ($details) {
 
-		$title = Helper::lang('notification.registration_email_title');
-        $name = (empty($external['first_name']) ? $external['user_name'] : $external['first_name']);
-        $link = '<a href="' . $hook->container->url('/') . '?verify-account=' . $external['token'] . '">
-            ' . Helper::lang('base.verify_email') . '
-        </a>';
-        $body = str_replace(
-            ['[USER]', '[VERIFY_LINK]'], 
-            [$name, $link], 
-            (string) Helper::lang('notification.registration_email_body')
-        );
-
-        $email = $hook->addEmail([
-            'title' => $title,
-            'body' => $body,
-            'recipient' => $external['user_name'],
-            'recipient_email' => $external['email'],
-            'recipient_id' => $external['id'],
-            'token' => $external['token']
-        ]);
-            
-        $notification = $hook->notificationsModel->insert([
-            'user_id'       => $external['id'],
-            'type'          => 'registration',
-            'created_at'    => time()
-        ]);
-
-        if ($email AND $notification)
-        	return true;
-        elseif ($email OR $notification)
-        	return false;
-        else
-        	return null;
-
-	},
-
+        return [
+            'notification' => [
+                'title' => 'notification.welcome_title',
+                'body' => 'notification.welcome_body',
+                'url' => null,
+                'parameters' => [],
+            ],
+            'email' => [
+                'title' => 'notification.welcome_email_title',
+                'body' => 'notification.welcome_email_body',
+                'parameters' => [
+                    'user' => $details['u_name'],
+                    'link_url' => '/auth/verify-account?token=' . $details['token'],
+                    'link_text' => 'auth.verify_account',
+                ]
+            ]
+        ];
+    },
+    /*
     // Account Recovery Email
-    'recovery_request' => function($hook, $external = null) {
+    'recovery_request' => function ($hook, $external = null) {
 
         $external = (array) $external;
         $title = Helper::lang('notification.recovery_request_email_title');
@@ -55,7 +40,7 @@ return [
             ' . Helper::lang('base.recovery_account') . '
         </a>';
         $body = str_replace(
-            ['[USER]', '[RECOVERY_LINK]'], 
+            ['[USER]', '[RECOVERY_LINK]'],
             [$name, $link],
             (string) Helper::lang('notification.recovery_request_email_body')
         );
@@ -76,13 +61,13 @@ return [
     },
 
     // Account Recovered
-    'account_recovered' => function($hook, $external = null) {
+    'account_recovered' => function ($hook, $external = null) {
 
         $external = (array) $external;
         $title = Helper::lang('notification.account_recovered_email_title');
         $name = (empty($external['first_name']) ? $external['user_name'] : $external['first_name']);
         $body = str_replace(
-            ['[USER]'], 
+            ['[USER]'],
             [$name],
             (string) Helper::lang('notification.account_recovered_email_body')
         );
@@ -102,16 +87,16 @@ return [
             'created_at'    => time()
         ]);
 
-        if ($email AND $notification)
+        if ($email and $notification)
             return true;
-        elseif ($email OR $notification)
+        elseif ($email or $notification)
             return false;
         else
             return null;
     },
 
     // Emal Change -> Again Verify
-    'email_change' => function($hook, $external = null) {
+    'email_change' => function ($hook, $external = null) {
 
         $title = Helper::lang('notification.email_change_email_title');
         $name = (empty($external['first_name']) ? $external['user_name'] : $external['first_name']);
@@ -119,7 +104,7 @@ return [
             ' . Helper::lang('base.verify_email') . '
         </a>';
         $body = str_replace(
-            ['[USER]', '[VERIFY_LINK]', '[CHANGES]'], 
+            ['[USER]', '[VERIFY_LINK]', '[CHANGES]'],
             [$name, $link, $external['changes']],
             (string) Helper::lang('notification.email_change_email_body')
         );
@@ -137,7 +122,6 @@ return [
             return true;
         else
             return null;
-
-    },
+    }, */
 
 ];
