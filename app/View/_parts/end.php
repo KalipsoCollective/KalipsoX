121efea433abc64db3cf6819755b7bc38e390e17
halillusion <halillusion@gmail.com>
2024-02-26 17:54:58 +0300

    Add Litepicker library

--- app/View/_parts/end.php
+++ app/View/_parts/end.php
@@ -4,6 +4,7 @@
     <script src="<?php echo $Helper::assets('libs/tabler/tabler.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('libs/nprogress/nprogress.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('libs/toastify/toastify.min.js'); ?>"></script>
+    <script src="<?php echo $Helper::assets('libs/litepicker/litepicker.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('js/script.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('js/app.js'); ?>"></script>
     </body>
