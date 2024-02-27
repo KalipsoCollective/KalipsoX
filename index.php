0f8cbfa4be978db525182fa4ed8fb5b8a84268e8
halillusion <halillusion@gmail.com>
2024-02-27 21:21:24 +0300

    Add autoload and error logging configuration

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 
 declare(strict_types=1);
 
+
+
+require __DIR__ . '/vendor/autoload.php';
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-require __DIR__ . '/vendor/autoload.php';
-// require __DIR__ . '/app/bootstrap.php';
 
 use KX\Core\Request;
 use KX\Core\Response;
@@ -21,6 +22,8 @@ define('KX_VERSION', 'alpha');
 try {
 
     $app = (new KX\Core\Factory)->setup();
+    ini_set('log_errors', 'On');
+    ini_set('error_log', KX\Core\Helper::path('app/Storage'));
 
     // $app->response->setLayout('layout'); // set global layout
     // $app->setLayout('layout');
@@ -52,15 +55,17 @@ try {
     /**
      * Route group
      **/
-    $app->routeGroup(['GET', '/auth', 'User@account', 'Auth@isAuthorized'], [
+    $app->routeGroup([['POST', 'GET'], '/auth', 'User@account', 'Auth@isLogged'], [
         [['POST', 'GET'], '/register', 'User@register', 'Auth@isNotLogged'],
         [['POST', 'GET'], '/verify-account', 'User@verifyAccount'],
         [['POST', 'GET'], '/login', 'User@login', 'Auth@isNotLogged'],
         [['POST', 'GET'], '/recovery', 'User@recovery', 'Auth@isNotLogged'],
-        [['POST', 'GET'], '/notifications', 'User@notifications', 'Auth@isAuthorized'],
-        [['POST', 'GET'], '/sessions', 'User@sessions', 'Auth@isAuthorized'],
+        [['POST', 'GET'], '/notifications', 'User@notifications', 'Auth@isLogged'],
+        [['POST', 'GET'], '/sessions', 'User@sessions', 'Auth@isLogged'],
         [['POST', 'GET'], '/logout', 'User@logout', 'Auth@isLogged'],
         [['POST', 'GET'], '/logout/:type', 'User@logout', 'Auth@isLogged'],
+        [['POST'], '/heartbeat', 'User@heartbeat', 'Auth@isLogged'],
+        [['POST'], '/notifications/:action/:id', 'User@notificationAction', 'Auth@isLogged'],
     ]);
 
     $app->routeGroup(['GET', '/dashboard', 'Panel@dashboard', 'Auth@isAuthorized'], [
