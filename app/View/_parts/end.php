d62b07bcee83e06b4b4bcf3d0f44f6f0c2e8150c
halillusion <halillusion@gmail.com>
2024-02-27 21:22:22 +0300

    Add new JavaScript libraries for date and time handling

--- app/View/_parts/end.php
+++ app/View/_parts/end.php
@@ -1,7 +1,10 @@
     <?php if (isset($layout['beforeBodyClose']) !== false) echo $layout['beforeBodyClose']; ?>
+    <script src="<?php echo $Helper::assets('libs/tabler/tabler.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('libs/jquery/jquery.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('libs/jquery-pjax/jquery.pjax.min.js'); ?>"></script>
-    <script src="<?php echo $Helper::assets('libs/tabler/tabler.min.js'); ?>"></script>
+    <script src="<?php echo $Helper::assets('libs/flatpickr/flatpickr.min.js'); ?>"></script>
+    <script src="<?php echo $Helper::assets('libs/flatpickr/locale/tr.min.js'); ?>"></script>
+    <script src="<?php echo $Helper::assets('libs/luxon/luxon.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('libs/nprogress/nprogress.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('libs/toastify/toastify.min.js'); ?>"></script>
     <script src="<?php echo $Helper::assets('libs/litepicker/litepicker.min.js'); ?>"></script>
