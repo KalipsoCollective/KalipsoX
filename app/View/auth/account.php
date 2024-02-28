54463adac8bb0abba0f3336af7241f49d78b38b3
halillusion <halillusion@gmail.com>
2024-02-28 21:43:26 +0300

    Add more notifications and improve session display

--- app/View/auth/account.php
+++ app/View/auth/account.php
@@ -26,9 +26,65 @@
                     switch ($section) {
                         case 'notifications':
                             echo $notificationList;
+                            if (!empty($notifications) && count($notifications) >= 20) {
+                                echo '<div class="card-footer bg-transparent mt-auto">
+                                    <div class="btn-list justify-content-center more-notifications">
+                                        <a href="javascript:;" data-kx-action="' . $Helper::base('auth/notifications?page=2') . '" class="btn btn-primary">
+                                            <span class="btn-text">' . $Helper::lang('base.more') . '</span>
+                                        </a>
+                                    </div>
+                                </div>';
+                            }
                             break;
                         case 'sessions':
-                            echo 's';
+                            echo '
+                            <div class="card-body">
+                                <h3 class="card-title">' . $Helper::lang('auth.sessions') . '</h3>
+                                <p class="card-subtitle">' . $Helper::lang('auth.sessions_desc') . '</p>';
+
+                            if (!empty($sessions)) {
+                                echo '<div class="list-group list-group-flush list-group-hoverable">';
+                                global $kxAuthToken;
+                                foreach ($sessions as $session) {
+
+                                    $session->header  = $Helper::userAgentDetails($session->header);
+
+                                    echo '
+                                    <div class="list-group-item sessions-' . $session->id . '">
+                                        ' . ($kxAuthToken === $session->auth_token ? '<div class="ribbon ribbon-top ribbon-start bg-green"><i class="ti ti-star"></i></div>' : '') . '
+                                        <div class="row align-items-center">
+                                            <div class="col-auto"><span class="status-dot d-block' . ($session->last_act_at > strtotime('-5 minutes') ? ' status-dot-animated bg-green' : '') . '"></span></div>
+                                                <div class="col">
+                                                    <div class="text-body d-inline-block" title="' . $session->header['user_agent'] . '">
+                                                        <i class="' . $session->header['b_icon'] . '"></i> ' . $session->header['browser'] . ' ' . $session->header['version'] . '
+                                                    </div>
+                                                    <time class="ms-2 timeago badge badge-outline text-blue" datetime="' . date('c', (int)$session->last_act_at) . '">' . date('d.m H:i', (int)$session->last_act_at) . '</time>
+                                                    <div class="d-block text-secondary mt-1">
+                                                        <i title="' . $session->auth_token . '" class="ti ti-fingerprint"></i> ' . $session->ip . ' &middot; 
+                                                        <i title="' . $session->header['platform'] . '" class="' . $session->header['p_icon'] . '"></i> ' . $session->header['os'] . ' &middot;
+                                                        <i class="ti ti-eye-pin"></i> <kbd>' . $session->last_act_on . '</kbd>
+                                                    </div>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <a href="javascript:;" data-kx-again data-kx-action="' . $Helper::base('auth/logout/' . $session->id) . '" class="list-group-item-actions">
+                                                        <i class="ti ti-power icon"></i>
+                                                    </a>
+                                                </div>
+                                        </div>
+                                    </div>';
+                                }
+                                echo '</div>';
+                            } else {
+                                echo '<p class="text-muted">' . $Helper::lang('base.no_more_data') . '</p>';
+                            }
+                            echo '</div>
+                            <div class="card-footer bg-transparent mt-auto">
+                                <div class="btn-list justify-content-end">
+                                    <a href="javascript:;" data-kx-again data-kx-action="' . $Helper::base('auth/logout/all') . '" class="btn btn-danger">
+                                        <span class="btn-text"><i class="ti ti-imp icon"></i> ' . $Helper::lang('auth.sessions_delete_all') . '</span>
+                                    </a>
+                                </div>
+                            </div>';
                             break;
                         default:
                     ?> <form data-kx-form action="<?php echo $Helper::base('auth'); ?>" method="post">
