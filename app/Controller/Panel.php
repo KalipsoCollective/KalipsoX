5656c434ef1023ed9c224f862ba083c64d3d7dff
halillusion <halillusion@gmail.com>
2024-02-28 21:42:18 +0300

    Add tableData method to Panel controller

--- app/Controller/Panel.php
+++ app/Controller/Panel.php
@@ -18,6 +18,8 @@ use KX\Core\Response;
 
 use KX\Controller\Notification;
 
+use KX\Helper\SSP;
+
 final class Panel
 {
 
@@ -55,5 +57,45 @@ final class Panel
             'description' => Helper::lang('base.user_roles_desc'),
             'auth' => $request->getMiddlewareParams(),
         ], 'layout');
+    }
+
+    public function tableData(Request $request, Response $response)
+    {
+
+        global $kxVariables;
+
+        extract(Helper::input([
+            'table' => 'nulled_text',
+        ], $request->getRouteDetails()->attributes));
+
+        if (empty($table)) {
+            return $response->json(['error' => 'Table name is required']);
+        } else if (!isset($kxVariables['datatables']['tables'][$table])) {
+            return $response->json(['error' => 'Table not found']);
+        } else {
+            $tableDetails = $kxVariables['datatables']['tables'][$table];
+
+            $columns = [];
+            $i = 0;
+            foreach ($tableDetails['columns'] as $tKey => $column) {
+                if ($tKey === 'actions') {
+                    continue;
+                }
+                $columns[] = [
+                    'db' => $tKey,
+                    'dt' => $i,
+                ];
+                $i++;
+            }
+
+            $sqlSelect = '(
+                SELECT *, role_id as role FROM users
+            ) as result';
+
+            $model = new Model();
+            $ssp = SSP::simple_extend($_POST, $model->pdo, 'id', $columns, $sqlSelect);
+
+            return $response->json($ssp);
+        }
     }
 }
