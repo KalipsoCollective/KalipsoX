53f063c9decb7fe5266af3c3c159ee12c5b4548e
halillusion <halillusion@gmail.com>
2024-02-28 21:43:29 +0300

    Add user management functionality to panel users page

--- app/View/panel/users.php
+++ app/View/panel/users.php
@@ -1,3 +1,19 @@
 <div class="page-body">
+    <div class="container-xl">
+        <div class="row row-cards">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"><?php echo $Helper::lang('base.users'); ?></h3>
 
+                        <div class="card-options">
+                            <a href="<?php echo $Helper::base('dashboard/users/add'); ?>" class="btn btn-sm btn-primary"><?php echo $Helper::lang('base.add_user'); ?></a>
+                        </div>
+
+                    </div>
+                    <?php echo \KX\Helper\HTML::tableLayout('users'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
