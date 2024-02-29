59edbc8afbe498f0705ff60b6c6021bff14f2d8d
halillusion <halillusion@gmail.com>
2024-02-29 20:46:44 +0300

    Refactor sidebar_routes.php to improve code structure

--- app/External/sidebar_routes.php
+++ app/External/sidebar_routes.php
@@ -1,24 +1,24 @@
 <?php
 
 return [
-  'dashboard' => [
-    'icon' => 'ti ti-layout-dashboard',
-    'link' => '/dashboard',
-    // 'badge' => '<span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>'
-  ],
-  'users' => [
-    'icon' => 'ti ti-users',
-    'children' => [
-      'users' => [
-        'link' => '/dashboard/users',
-      ],
-      'user_roles' => [
-        'link' => '/dashboard/user-roles',
-      ],
+    'dashboard' => [
+        'icon' => 'ti ti-layout-dashboard',
+        'link' => '/dashboard',
+        // 'badge' => '<span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>'
     ],
-  ],
-  'settings' => [
-    'icon' => 'ti ti-settings',
-    'link' => '/dashboard/settings',
-  ],
+    'users' => [
+        'icon' => 'ti ti-users',
+        'children' => [
+            'users' => [
+                'link' => '/dashboard/users',
+            ],
+            'user_roles' => [
+                'link' => '/dashboard/user-roles',
+            ],
+        ],
+    ],
+    'settings' => [
+        'icon' => 'ti ti-settings',
+        'link' => '/dashboard/settings',
+    ],
 ];
