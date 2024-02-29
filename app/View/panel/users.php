583674d3c2e959f2da9c7e6493c984e52271b80a
halillusion <halillusion@gmail.com>
2024-02-29 20:47:06 +0300

    Refactor user panel to use modal for adding users

--- app/View/panel/users.php
+++ app/View/panel/users.php
@@ -5,11 +5,13 @@
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title"><?php echo $Helper::lang('base.users'); ?></h3>
-
                         <div class="card-options">
-                            <a href="<?php echo $Helper::base('dashboard/users/add'); ?>" class="btn btn-sm btn-primary"><?php echo $Helper::lang('base.add_user'); ?></a>
+                            <?php if ($Helper::authorization('dashboard/users/add')) : ?>
+                                <button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#addUserModal">
+                                    <?php echo $Helper::lang('base.add_user'); ?>
+                                </button>
+                            <?php endif; ?>
                         </div>
-
                     </div>
                     <?php echo \KX\Helper\HTML::tableLayout('users'); ?>
                 </div>
