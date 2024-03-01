93199751cd8fe68d9f5760ceaf082a876bd00101
halillusion <halillusion@gmail.com>
2024-03-01 21:08:26 +0300

    Add settings card to panel settings page

--- app/View/panel/settings.php
+++ app/View/panel/settings.php
@@ -1,3 +1,9 @@
 <div class="page-body">
-
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-12">
+                <?php echo $settingsCard; ?>
+            </div>
+        </div>
+    </div>
 </div>
