979b6810c30ff70d2bfdc99f1ea3afb4a3cbeea2
halillusion <halillusion@gmail.com>
2024-03-21 15:14:00 +0300

    Add sessions view to panel

---
+++ app/View/panel/sessions.php
@@ -0,0 +1,16 @@
+<div class="page-body">
+    <div class="container-xl">
+        <div class="row row-cards">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"><?php echo $Helper::lang('base.sessions'); ?></h3>
+                        <div class="card-options">
+                        </div>
+                    </div>
+                    <?php echo \KX\Helper\HTML::tableLayout('sessions'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
