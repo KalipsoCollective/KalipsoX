38acb0d47bd2b39e1bb48dc293ab36cd65522999
halillusion <halillusion@gmail.com>
2024-04-04 17:56:42 +0300

    Refactor Notifications model constructor and set schema

--- app/Model/Notifications.php
+++ app/Model/Notifications.php
@@ -15,71 +15,71 @@ use KX\Core\Helper;
 final class Notifications extends Model
 {
 
-  function __construct()
-  {
+    function __construct()
+    {
 
-    // set table name
-    $this->setTable('notifications');
+        // set table name
+        $this->setTable('notifications');
 
-    // set schema
-    $this->setSchema([
-      'columns' => [
-        'id' => [
-          'type' => 'int',
-          'length' => 11,
-          'nullable' => false,
-          'auto_increment' => true,
-          'primary' => true,
-        ],
-        'user_id' => [
-          'type' => 'int',
-          'length' => 11,
-          'nullable' => false,
-          'default' => '0',
-        ],
-        'type' => [
-          'type' => 'varchar',
-          'length' => 250,
-          'nullable' => false,
-        ],
-        'details' => [
-          'type' => 'text',
-        ],
-        'created_at' => [
-          'type' => 'varchar',
-          'length' => 150,
-        ],
-        'viewed_at' => [
-          'type' => 'varchar',
-          'length' => 150,
-          'nullable' => true,
-        ],
-        'deleted_at' => [
-          'type' => 'varchar',
-          'length' => 150,
-          'nullable' => true,
-        ],
-        'status' => [
-          'type' => 'enum(\'active\',\'viewed\',\'deleted\')',
-          'nullable' => false,
-          'default' => 'active'
-        ]
-      ],
-      'indexes' => [
-        'primary_key' => [
-          'type' => 'primary',
-          'columns' => ['id'],
-        ],
-        'indexed_fields' => [
-          'type' => 'index',
-          'columns' => ['user_id', 'type', 'status'],
-        ],
-      ],
-      'engine' => 'InnoDB',
-      'charset' => 'utf8mb4',
-      'collation' => 'utf8mb4_unicode_520_ci',
-    ]);
+        // set schema
+        $this->setSchema([
+            'columns' => [
+                'id' => [
+                    'type' => 'int',
+                    'length' => 11,
+                    'nullable' => false,
+                    'auto_increment' => true,
+                    'primary' => true,
+                ],
+                'user_id' => [
+                    'type' => 'int',
+                    'length' => 11,
+                    'nullable' => false,
+                    'default' => '0',
+                ],
+                'type' => [
+                    'type' => 'varchar',
+                    'length' => 250,
+                    'nullable' => false,
+                ],
+                'details' => [
+                    'type' => 'text',
+                ],
+                'created_at' => [
+                    'type' => 'varchar',
+                    'length' => 150,
+                ],
+                'viewed_at' => [
+                    'type' => 'varchar',
+                    'length' => 150,
+                    'nullable' => true,
+                ],
+                'deleted_at' => [
+                    'type' => 'varchar',
+                    'length' => 150,
+                    'nullable' => true,
+                ],
+                'status' => [
+                    'type' => 'enum(\'active\',\'viewed\',\'deleted\')',
+                    'nullable' => false,
+                    'default' => 'active'
+                ]
+            ],
+            'indexes' => [
+                'primary_key' => [
+                    'type' => 'primary',
+                    'columns' => ['id'],
+                ],
+                'indexed_fields' => [
+                    'type' => 'index',
+                    'columns' => ['user_id', 'type', 'status'],
+                ],
+            ],
+            'engine' => 'InnoDB',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_520_ci',
+        ]);
 
-    parent::__construct();
-  }
+        parent::__construct();
+    }
 }
